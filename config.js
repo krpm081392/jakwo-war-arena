54439b5be2f25d463a1c63a085a0cf5ac7ee05e2
krpm081392 <krpm081392@gmail.com>
2026-05-31 19:18:54 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.JAKWO_CONFIG = {
-  supabaseUrl: '',
-  supabaseAnonKey: '',
+  supabaseUrl: 'NEXT_PUBLIC_SUPABASE_ANON_KEY',
+  supabaseAnonKey: 'sb_publishable_ftx8e2Ml13aEAK43hlLz1Q_wQVvNqpB',
   twitter: 'https://x.com/jakw0o',
   telegram: 'https://t.me/+hJ-0IQGfYTRmYjM0',
   receiverWallet: '7YhjDDUMCq9eS2wRYv2rcLqFoSt7rN1N8FkG1CQavQHq',
