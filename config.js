9847b18f95df59110f60116f77cc3247521ee9e8
krpm081392 <krpm081392@gmail.com>
2026-06-06 14:00:40 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,10 +1,8 @@
 window.JAKWO_CONFIG = {
-  supabaseUrl: 'https://qfonfztznecycwfbpnhx.supabase.co',
-  supabaseAnonKey: 'sb_publishable_ftx8e2MlI3aEAK43hlLzlQ_wQWvNqpB',
+  supabaseUrl: '',
+  supabaseAnonKey: '',
   twitter: 'https://x.com/jakw0o',
   telegram: 'https://t.me/+hJ-0IQGfYTRmYjM0',
   receiverWallet: '7YhjDDUMCq9eS2wRYv2rcLqFoSt7rN1N8FkG1CQavQHq',
-  solanaRpc: 'https://api.mainnet-beta.solana.com',
-  usdcMint: 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v',
   adminPassword: 'JAKWOADMIN2026'
 };
